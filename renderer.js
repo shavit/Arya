ea2ba584d6cd75260f46043feb0af7e5c90116a6
shavit <shavit@users.noreply.github.com>
2017-12-06 16:31:58 -0500

    💬

---
+++ renderer.js
@@ -0,0 +1,19 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+// Webcam
+// const videoPreview = document.getElementById('videoPreview')
+// navigator.webkitGetUserMedia({
+//   audio: true,
+//   video: {
+//     mandantory: {
+//       maxHeight: 240,
+//       maxWidth: 320,
+//       minHeight: 240,
+//       minWidth: 320,
+//     }
+//   }
+// },
+// stream => videoPreview.src = window.URL.createObjectURL(stream),
+// error => console.log(error))
